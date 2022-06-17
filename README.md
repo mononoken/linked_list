8d5f857a74208fef6d7d91bcd7fd2c3acd9112ac
mononoken <ken.maeshima@pm.me>
2022-06-17 16:50:36 -0700

    Setup README.md and get first methods complete for LinkedList

---
+++ README.md
@@ -0,0 +1,3 @@
+https://www.theodinproject.com/lessons/ruby-linked-lists
+
+This is an exercise from The Odin Project. In this exercise, a linked list class will be created to demonstrate understanding of the mechanics of a linked list data structure.
---
+++ lib/linked_list.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+# Node for use with a LinkedList.
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+# List of connected Nodes
+class LinkedList
+  attr_reader :head, :tail
+
+  def initialize(head_value, *node_values)
+    @head = Node.new(head_value)
+
+    previous_node = @head
+    node_values.each do |node_value|
+      current_node = Node.new(node_value)
+      previous_node.next_node = current_node unless previous_node.nil?
+      previous_node = current_node
+    end
+
+    @tail = previous_node
+  end
+
+  def append(value)
+    append_node = Node.new(value)
+    @tail.next_node = append_node
+    @tail = append_node
+  end
+
+  def prepend(value)
+    prepend_node = Node.new(value, @head)
+    @head = prepend_node
+  end
+
+  def size
+    size = 0
+    current_node = @head
+    until current_node.nil?
+      size += 1
+      current_node = current_node.next_node
+    end
+    size
+  end
+
+  def at(index)
+    if index == 0
+      @head
+    else
+      current_node = @head
+      index.times do
+        current_node = current_node.next_node
+      end
+      current_node
+    end
+  end
+
+  def pop
+    # Remove last element from the list
+    previous_tail = @tail
+    current_node = @head
+    (size - 2).times do
+      current_node = current_node.next_node
+    end
+    current_node.next_node = nil
+    @tail = current_node
+    previous_tail
+  end
+
+  def contains?(value)
+    current_node = @head
+    until current_node.nil? || current_node.value == value
+      current_node = current_node.next_node
+    end
+    return false if current_node.nil?
+
+    current_node.value == value
+  end
+
+  def find(value)
+  end
+
+  def to_s
+  end
+
+  def insert_at(value, index)
+  end
+
+  def remove_at(index)
+  end
+end
